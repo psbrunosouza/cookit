ab2effee1da1f69daae454bc2a5eeb44aad68b6f
psbrunosouza <psbrunosouza@gmail.com>
2021-06-05 20:28:19 -0300

    clear local ingredient data when ingredient component unmounted

--- src/reducer/ingredientReducer.ts
+++ src/reducer/ingredientReducer.ts
@@ -1,8 +1,11 @@
 
 const ingredientReducer = (state: any = [], action: any) => {
   switch (action.type) {
-    case "ADD_INGREDIENT":
-      return [...state, action.payload];
+    case "INSERT_INGREDIENT":
+      return [...state, action.payload]
+
+    case "CREATE_INGREDIENTS_LIST":
+      return state = action.payload;
 
     case "REMOVE_INGREDIENT":
       return state.filter((state: any) => state.id !== action.payload);
--- src/services/StepService.ts
+++ src/services/StepService.ts
@@ -4,37 +4,7 @@ import { IStep } from "../models/Step";
 
 class StepService {
 
-  async create(recipe: IRecipes, steps: IStep[] ): Promise<void> {
-    try{
-      //const object = await AsyncStorage.getItem(recipeId) as string;
-      //const recipe: IRecipes = JSON.parse(object);
-
-      steps.forEach((step) => {
-        recipe.steps.push(step)
-      });
-
-      await AsyncStorage.setItem(recipe.id, JSON.stringify(recipe));
-    }catch(e){
-      console.log(e);
-    }
-  }
-
-  async update(recipeId: string, steps: IStep[] ): Promise<void> {
-    try{
-      const object = await AsyncStorage.getItem(recipeId) as string;
-      const recipe: IRecipes = JSON.parse(object);
-
-      recipe.steps = [];
-
-      steps.forEach((step) => {
-        recipe.steps.push(step)
-      });
-
-      await AsyncStorage.setItem(recipeId, JSON.stringify(recipe));
-    }catch(e){
-      console.log(e);
-    }
-  }
+  
 }
 
 export { StepService }
